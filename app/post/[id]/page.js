111118c961440f03b46bb7f957b3c98b692b90b1
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2024-10-16 13:50:07 +0300

    export added

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,6 +1,14 @@
 import Post from "@/app/components/Post";
 
 
+export async function generateStaticParams() {
+    const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json())
+
+    return posts.map((post) => ({
+        id: post.id.toString(),
+    }))
+}
+
 export async function generateMetadata({ params, searchParams }) {
     const post = await fetchData(params.id)
 
